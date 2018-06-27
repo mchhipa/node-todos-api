fe971e73d4563e6ea61b307f34c2fcc437c57857
Mehboob Chhipa <mehboob@chhipa.com>
2018-06-27 18:44:15 +0300

    added get todo with id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID} =require('mongodb');
 
-const {mongoose} = require('./db/mongoose');
+const { mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
 
@@ -16,7 +17,7 @@ app.post('/todos',(req,res)=>{
         res.send(result);
         console.log(result);
     },(err)=>{
-        res.status(400).send(err);
+        res.statusStatus(400).send(err);
         throw new Error("Error Saving todo",err)
     })
 });
@@ -24,11 +25,24 @@ app.get('/todos',(req,res)=>{
     Todo.find({}).then((todos)=>{
         res.send(todos);
     },(err)=>{
-        res.send(400);
+        res.sendStatus(400);
         res,send(err);
         //throw new Error("error fetching")
     })
-})
+});
+app.get('/todo/:id',(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+       return  res.sendStatus(404).end();
+    }
+    Todo.findById(req.params.id)
+        .then((todo)=> {
+            if(!todo){
+                return res.sendStatus(404).send();
+            }
+            res.send(todo)
+        });
+        
+});
 app.listen('3000',()=>{
     console.log('started server on 3000');
 })
